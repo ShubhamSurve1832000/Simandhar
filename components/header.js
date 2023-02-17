bd6235b85ea8a5e83e357af6afc5b3554832231e
ShubhamSurve1832000 <123547647+ShubhamSurve1832000@users.noreply.github.com>
2023-02-17 12:25:22 +0530

    Add files via upload
    header js added

--- components/header.js
+++ components/header.js
@@ -34,21 +34,21 @@ export default function header() {
     slidesToShow: 5,
     slidesToScroll: 1,
     responsive: [
-    {
+      {
         breakpoint: 600,
         settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
         }
-    },
-    {
+      },
+      {
         breakpoint: 480,
         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
-    }
+      }
     ]
   };
 
@@ -56,7 +56,7 @@ export default function header() {
   // const toggleClass = () => {
   //   setActive(!isActive);
   // };
-  
+
   // const [porgrams, setPrograms] = useState([{ name: 'Our Programs', icon: '<IoCaretForward />' }, { name: 'US CPA' }, { name: 'EA' }, { name: 'CIA' }, { name: 'IFRS' }, { name: 'Data Analytics' }, { name: 'Simandhar Saamarth' }, { name: 'More' }])
 
 
@@ -67,38 +67,38 @@ export default function header() {
       <header className="header" id="header">
         <nav className='navbar hf-container'>
           <div className='logo_img  resp-img-box'>
-            <Image src='/img/logo.png' layout="fill" className='resp-img'  alt='Simandhar logo'/>
-            <Image src='/img/mobile_logo.png' layout="fill" className='resp-img mobile-logo'  alt='Simandhar logo'/>
+            <Image src='/img/logo.png' layout="fill" className='resp-img' alt='Simandhar logo' />
+            <Image src='/img/mobile_logo.png' layout="fill" className='resp-img mobile-logo' alt='Simandhar logo' />
           </div>
           <div className='left_nav'>
-          <button className="menu-btn" >
-                <span></span>
-                <span></span>
-                <span></span>
+            <button className="menu-btn" >
+              <span></span>
+              <span></span>
+              <span></span>
             </button>
             <div class="nav">
-            <button class="close-menu">
-                    <span></span>
-                    <span></span>
-                </button>
-            <ul className='navbar_list'>
-              {/* {
+              <button class="close-menu">
+                <span></span>
+                <span></span>
+              </button>
+              <ul className='navbar_list'>
+                {/* {
                 links.map(link => (
                   <li>
                     <Link className="home" href={link.path}>{link.label}</Link>
                   </li>
                 ))
               } */}
-              <li className='active'>Home</li>
-              <li>About</li>
-              <li>Webinars</li>
-              <li>Free Resources</li>
-              <li className='more_dropdown'>
-                More
-                <span><i><RiArrowDropDownLine /></i></span>
-          
-              </li>
-            </ul>
+                <li className='active'>Home</li>
+                <li>About</li>
+                <li>Webinars</li>
+                <li>Free Resources</li>
+                <li className='more_dropdown'>
+                  More
+                  <span><i><RiArrowDropDownLine /></i></span>
+
+                </li>
+              </ul>
             </div>
             <div className='btn01'><button className='btn'>Buy Course</button></div>
             <span><i className='profile'> <CgProfile /></i></span>
@@ -122,48 +122,50 @@ export default function header() {
 
         {/* Second NavBar */}
         <section className='backgroud'>
-      
+
           <div className='program_bar'>
             <div className='nav-title'>Our Programs<span><i><IoCaretForward /></i></span></div>
-            {/* <div className='program_bar'> */}
-            {/* <Slider {...programSlider}> */}
+              <div className='program-scroll'>
+            <div className='programs'>
+                {/* <Slider {...programSlider}> */}
 
-            <div className='nav-title'><h3>US CPA</h3></div>
-            <div className='nav-title'>US CMA</div>
-            <div className='nav-title'>EA</div>
-            <div className='nav-title'>CIA</div>
-            <div className='nav-title'>IFRS</div>
-            <div className='nav-title'>Data Analytics</div>
-            <div className='nav-title'>Simandhar Saamarth</div>
-            {/* </Slider> */}
+                <div className='nav-title'><h3>US CPA</h3></div>
+                <div className='nav-title'>US CMA</div>
+                <div className='nav-title'>EA</div>
+                <div className='nav-title'>CIA</div>
+                <div className='nav-title'>IFRS</div>
+                <div className='nav-title'>Data Analytics</div>
+                <div className='nav-title'>Simandhar Saamarth</div>
+                {/* </Slider> */}
 
-            {/* <div className='nav-title'>More<span><i><RiArrowDropDownLine /></i></span>
+                {/* <div className='nav-title'>More<span><i><RiArrowDropDownLine /></i></span>
             </div> */}
+              </div>
+            </div>
           </div>
-          {/* </div> */}
-        
+
         </section>
 
 
         {/* Offer Row */}
         <section>
-        <div className='offer_row conatainer02'>
-          <div className='offer_box1 resp-img-box'>
-            <Image src='/img/flash_sale.png' layout="fill" className='resp-img' alt=''/>
+          <div className='offer_row conatainer02'>
+            <div className='offer_box1 resp-img-box'>
+              <Image src='/img/flash_sale.png' layout="fill" className='resp-img' alt='' />
+            </div>
+            <div className='offer_box2'>
+              <h3 >FLAT 50% OFF</h3>
+            </div>
+            <div className='offer_box3'>
+              <h3>Discount <br /> on All Courses</h3>
+            </div>
+            <div className='offer_box4'>
+              <h3>Promo Code : SALE 50</h3>
+            </div>
+            <div className='offer_box5'>
+              <h3>Buy Now</h3>
+            </div>
           </div>
-          <div className='offer_box2'>
-            <h3 >FLAT 50% OFF</h3>
-          </div>
-          <div className='offer_box3'>
-            <h3>Discount <br /> on All Courses</h3>
-          </div>
-          <div className='offer_box4'>
-            <h3>Promo Code : SALE 50</h3>
-          </div>
-          <div className='offer_box5'>
-            <h3>Buy Now</h3>
-          </div>
-        </div>
         </section>
       </header>
 
