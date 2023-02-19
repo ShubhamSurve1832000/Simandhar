9dda9bc7bcad26a30ffa2a51322c262b8b787d00
ShubhamSurve1832000 <123547647+ShubhamSurve1832000@users.noreply.github.com>
2023-02-19 17:08:30 +0530

    Add files via upload
    offer section added

---
+++ home/OfferSection.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+
+const OfferSection = () => {
+    return (
+        <section>
+            <div className='offer_row conatainer02'>
+                <div className='offer_box1 resp-img-box'>
+                    <Image src='/img/flash_sale.png' layout="fill" className='resp-img' alt='' />
+                </div>
+                <div className='offer_box2'>
+                    <h3 >FLAT 50% OFF</h3>
+                </div>
+                <div className='offer_box3'>
+                    <h3>Discount <br /> on All Courses</h3>
+                </div>
+                <div className='offer_box4'>
+                    <h3>Promo Code : SALE 50</h3>
+                </div>
+                <div className='offer_box5'>
+                    <h3>Buy Now</h3>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default OfferSection
