6eca3b8e4630a8e6045edde3cd08bb56aca78b8a
ShubhamSurve1832000 <123547647+ShubhamSurve1832000@users.noreply.github.com>
2023-02-21 16:17:57 +0530

    Add files via upload
    app page added

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,18 @@
 //import type { AppProps } from 'next/app'
 //import '../styles/header-footer.css'
 import '../styles/style.scss'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 //export default function App({ Component, pageProps }: AppProps) {
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-
+  return(
+    <>
+    <Header/>
+    <Component {...pageProps} /> 
+    <Footer />
+    </>
+    )
 
 }
 
