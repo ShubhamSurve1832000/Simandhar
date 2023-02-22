d942d4f4a6c177051afbedd0eb3ee4523f42c224
ShubhamSurve1832000 <123547647+ShubhamSurve1832000@users.noreply.github.com>
2023-02-22 15:00:36 +0530

    Add files via upload
    done

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,17 @@
 import '../styles/style.scss'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import StickyComponents from '../components/StickyComponents'
+import Image from 'next/image'
 
 //export default function App({ Component, pageProps }: AppProps) {
 export default function App({ Component, pageProps }) {
   return(
     <>
     <Header/>
+    <StickyComponents />
+
+    <a href='#' className='chat-icon'><Image src="/img/chat.png" alt="" width={122} height={135} /></a>
     <Component {...pageProps} /> 
     <Footer />
     </>
