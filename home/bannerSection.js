9d120d5b57d9c1016727f85f8f25ac72aa8ff199
ShubhamSurve1832000 <123547647+ShubhamSurve1832000@users.noreply.github.com>
2023-02-27 12:47:28 +0530

    Add files via upload
    home file added

--- home/bannerSection.js
+++ home/bannerSection.js
@@ -1,23 +1,60 @@
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Slider from 'react-slick'
 
-export default function bannerSection() {
-  // const [isActive, setActive] = useState(false);
-  // const toggleClass = () => {
-  //   setActive(!isActive);
-  // };
+
+const bannerSection = ({showPopup}) => {
+  
+  var bannerSlider = {
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+    arrows: false,
+    autoplay:true,
+    autoplaySpeed:5000,
+	};
 
   return (
     <>
       <section className="banner-section flex">
         <div className="right-box">
-          <div className="banner-content">
-            <div class="banner-text">
-              <h1 className='banner-title'>Get into <span>Big 4s and MNCs</span> with the most trusted institute of India</h1>
-              <p>We believe that education has the ability to transform every individual.</p>
+          <Slider {...bannerSlider}>
+            <div className='banner-slider'>
+              <div className="banner-inner-slider" style={{backgroundImage: 'url("img/banner_bg.jpg")'}}>
+              <div className="banner-content">
+                  <div className="banner-text">
+                    <h1 className='banner-title'>Get into <span>Big 4s and MNCs</span> with the most trusted institute of India</h1>
+                    <p>We believe that education has the ability to transform every individual.</p>
+                  </div>
+                  <button className='btn maroon-btn' onClick={showPopup}>Enquire Now</button>
+                </div>
+              </div>
             </div>
-            <a href="#" className='btn maroon-btn'>Enquire Now</a>
-          </div>
+            <div className='banner-slider'>
+              <div className="banner-inner-slider" style={{backgroundImage: 'url("img/banner_bg.jpg")'}}>
+                <div className="banner-content">
+                  <div className="banner-text">
+                    <h1 className='banner-title'>Get into <span>Big 4s and MNCs</span> with the most trusted institute of India</h1>
+                    <p>We believe that education has the ability to transform every individual.</p>
+                  </div>
+                  <button className='btn maroon-btn' onClick={showPopup}>Enquire Now</button>
+                </div>
+              </div>
+            </div>
+            <div className='banner-slider'>
+              <div className="banner-inner-slider" style={{backgroundImage: 'url("img/banner_bg.jpg")'}}>
+                <div className="banner-content">
+                  <div className="banner-text">
+                    <h1 className='banner-title'>Get into <span>Big 4s and MNCs</span> with the most trusted institute of India</h1>
+                    <p>We believe that education has the ability to transform every individual.</p>
+                  </div>
+                  <button className='btn maroon-btn' onClick={showPopup}>Enquire Now</button>
+                </div>
+              </div>
+            </div>
+          </Slider>
         </div>
         <div className="left-box resp-img-box">
           <h3 className='heading04'>Learn Anywhere with Online Courses</h3>
@@ -26,51 +63,10 @@ export default function bannerSection() {
           <div className="clear"></div>
         </div>
       </section>
-      {/* <div class="sticky-box">
-        <a href="#" className='resp-img-box'><Image src="/img/phone.png" alt="" layout="fill" className='resp-img' /></a>
-        <a href="#" className='resp-img-box'><Image src="/img/email.png" alt="" layout="fill" className='resp-img' /></a>
-        <a href="#" className='wa-icon resp-img-box'><Image src="/img/whatsapp.png" alt="" layout="fill" className='resp-img' /></a>
-        <button className={'enq-btn resp-img-box' + ' ' + (isActive ? 'active' : '')} onClick={toggleClass} >
-          <Image src="/img/enquire_now.svg" alt="" layout="fill" className='resp-img' />
-        </button>
-      </div> */}
-      {/* <div className={'enq-form' + ' ' + (isActive ? 'active' : '')}>
-        <button className='close-form resp-img-box' onClick={toggleClass}><Image src="/img/form_arrow.svg" alt="" layout="fill" className='resp-img' /></button>
-        <div className='form-header'>
-          <i className='resp-img-box'><Image src="/img/enq_icon.svg" layout="fill" className='resp-img' /></i>
-          <p>We assure a Price match <br /> Guarantee with any Becker Partner</p>
-        </div>
-        <div className='form-box'>
-          <h6>Take The First Step!</h6>
-          <form>
-            <div className='input-box'>
-              <input type="text" placeholder='Enter Your Name' />
-            </div>
-            <div className='input-box'>
-              <input type="text" placeholder='Enter Your Email Address' />
-            </div>
-            <div className='input-box'>
-              <input type="text" placeholder='Enter your Mobile Number' />
-            </div>
-            <div className='input-box custom-select'>
-              <select id="select-course" >
-                <option>Select Course</option>
-                <option value="Course 1">US CPA</option>
-                <option value="Course 2">US CMA</option>
-                <option value="Course 3">EA</option>
-                <option value="Course 4">CIA</option>
-                <option value="Course 4">IFRS</option>
-                <option value="Course 4">Data Analytics</option>
-                <option value="Course 4">Simandhar Saamarth</option>
-              </select>
-            </div>
-            <div className='input-box'>
-              <button className='btn black-border black-border-btn-arrow'>Submit</button>
-            </div>
-          </form>
-        </div>
-      </div>
-      <div class="overlay" onClick={toggleClass} style={{ display: (isActive ? "block" : 'none') }}></div> */}
+      
     </>
   )
 }
+
+
+export default bannerSection;
